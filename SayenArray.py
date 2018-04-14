ec01ad1fe3f39c9bbd58211b24302be78079f04e
JoyOfYahweh <34731705+JoyOfYahweh@users.noreply.github.com>
2018-04-14 14:29:49 +0800

    Add files via upload

---
+++ SayenArray.py
@@ -0,0 +1,10 @@
+Sayen_list = ['S','A','Y','E','N']
+print(Sayen_list[0])
+print(Sayen_list[1])
+print(Sayen_list[2])
+print(Sayen_list[3])
+
+#Nested
+S_list = ["Pogi", [1,9,9,4]]
+print(S_list[0][1])
+print(S_list[1][3])
